2a8a36df6a7d74036c8897d702774b831da6443f
Candra Sudirman <dev.cndrsdrmn@gmail.com>
2025-10-04 13:52:19 +0700

    [1.x] Add test fixtures (#6)
    * test: introduce test helper `fixture` and `fixtures` for handle static files
    
    * test: add `fixtures/csv` for sampling data

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,28 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Get the file path from the fixture's directory.
+     */
+    protected function fixture(string $name): string
+    {
+        $path = __DIR__.DIRECTORY_SEPARATOR.'fixtures'.DIRECTORY_SEPARATOR.$name;
+
+        $this->assertFalse(is_dir($path));
+        $this->assertFileExists($path);
+
+        return $path;
+    }
+
+    /**
+     * Get the file paths from the fixture's directory.
+     *
+     * @return array<string>
+     */
+    protected function fixtures(string ...$names): array
+    {
+        return array_map([$this, 'fixture'], $names);
+    }
+}
